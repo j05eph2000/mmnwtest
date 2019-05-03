f3d776b676f65418f3430f0cbff6c22600623682
CryptoCatOkiOki <nuncij@gmail.com>
2019-05-03 13:22:17 +0200

    Update monk_setupv2.sh
    bootstrap is downloaded only once
    port and rpc port is setted automatically

--- monk_setupv2.sh
+++ monk_setupv2.sh
@@ -8,11 +8,26 @@ YELLOW='\033[1;33m'
 BROWN='\033[0;34m'
 NC='\033[0m' # No Color
 
+# CONFIGURATION
+NAME="monkey"
+NAMEALIAS="monk"
+WALLETVERSION="2.3.0"
+
+# ADDITINAL CONFIGURATION
+WALLETDLFOLDER="${NAME}-${WALLETVERSION}"
+WALLETDL="${WALLETDLFOLDER}-x86_64-linux-gnu.tar.gz"
+URL="https://github.com/MONKEYPROJECT/MonkeyV2/releases/download/v${WALLETVERSION}/${WALLETDL}"
+CONF_FILE="${NAME}.conf"
+CONF_DIR_TMP=~/"${NAME}_tmp"
+BOOTSTRAPURL="http://167.86.97.235/${NAMEALIAS}/bootstrap/bootstrap.zip"
+PORT=37234
+RPCPORT=9234
+
 cd ~
 echo "******************************************************************************"
 echo "* Ubuntu 16.04 is the recommended operating system for this install.         *"
 echo "*                                                                            *"
-echo "* This script will install and configure your MONK Coin masternodes (v2.3.0).*"
+echo "* This script will install and configure your ${NAME} Coin masternodes (v${WALLETVERSION}).*"
 echo "******************************************************************************"
 echo && echo && echo
 echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
@@ -23,72 +38,100 @@ echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
 echo && echo && echo
 
 if [[ $(lsb_release -d) != *16.04* ]]; then
-  echo -e "${RED}The operating system is not Ubuntu 16.04. You must be running on ubuntu 16.04.${NC}"
-  exit 1
+   echo -e "${RED}The operating system is not Ubuntu 16.04. You must be running on ubuntu 16.04.${NC}"
+   exit 1
 fi
+
+function get_ip() {
+  declare -a NODE_IPS
+  for ips in $(netstat -i | awk '!/Kernel|Iface|lo/ {print $1," "}')
+  do
+    NODE_IPS+=($(curl --interface $ips --connect-timeout 2 -s4 icanhazip.com))
+  done
+
+  if [ ${#NODE_IPS[@]} -gt 1 ]
+    then
+      echo -e "${GREEN}More than one IP. Please type 0 to use the first IP, 1 for the second and so on...${NC}"
+      INDEX=0
+      for ip in "${NODE_IPS[@]}"
+      do
+        echo ${INDEX} $ip
+        let INDEX=${INDEX}+1
+      done
+      read -e choose_ip
+      NODEIP=${NODE_IPS[$choose_ip]}
+  else
+    NODEIP=${NODE_IPS[0]}
+  fi
+}
+
+get_ip
+IP=NODEIP
 
 echo -e "${YELLOW}Do you want to install all needed dependencies (no if you did it before, yes if you are installing your first node)? [y/n]${NC}"
 read DOSETUP
 
 if [[ ${DOSETUP,,} =~ "y" ]] ; then
-  sudo apt-get update
-  sudo apt-get -y upgrade
-  sudo apt-get -y dist-upgrade
-  sudo apt-get install -y nano htop git
-  sudo apt-get install -y software-properties-common
-  sudo apt-get install -y build-essential libtool autotools-dev pkg-config libssl-dev
-  sudo apt-get install -y libboost-all-dev
-  sudo apt-get install -y libevent-dev
-  sudo apt-get install -y libminiupnpc-dev
-  sudo apt-get install -y autoconf
-  sudo apt-get install -y automake unzip
-  sudo add-apt-repository  -y  ppa:bitcoin/bitcoin
-  sudo apt-get update
-  sudo apt-get install -y libdb4.8-dev libdb4.8++-dev
-  sudo apt-get install -y dos2unix
-  sudo apt-get install -y jq
+   sudo apt-get update
+   sudo apt-get -y upgrade
+   sudo apt-get -y dist-upgrade
+   sudo apt-get install -y nano htop git
+   sudo apt-get install -y software-properties-common
+   sudo apt-get install -y build-essential libtool autotools-dev pkg-config libssl-dev
+   sudo apt-get install -y libboost-all-dev
+   sudo apt-get install -y libevent-dev
+   sudo apt-get install -y libminiupnpc-dev
+   sudo apt-get install -y autoconf
+   sudo apt-get install -y automake unzip
+   sudo add-apt-repository  -y  ppa:bitcoin/bitcoin
+   sudo apt-get update
+   sudo apt-get install -y libdb4.8-dev libdb4.8++-dev
+   sudo apt-get install -y dos2unix
+   sudo apt-get install -y jq
 
-  cd /var
-  sudo touch swap.img
-  sudo chmod 600 swap.img
-  sudo dd if=/dev/zero of=/var/swap.img bs=1024k count=2000
-  sudo mkswap /var/swap.img
-  sudo swapon /var/swap.img
-  sudo free
-  sudo echo "/var/swap.img none swap sw 0 0" >> /etc/fstab
-  cd
+   cd /var
+   sudo touch swap.img
+   sudo chmod 600 swap.img
+   sudo dd if=/dev/zero of=/var/swap.img bs=1024k count=2000
+   sudo mkswap /var/swap.img
+   sudo swapon /var/swap.img
+   sudo free
+   sudo echo "/var/swap.img none swap sw 0 0" >> /etc/fstab
+   cd
 
-  ## COMPILE AND INSTALL
-  mkdir -p ~/monkey_tmp
-  cd ~/monkey_tmp
-  
-  wget https://github.com/MONKEYPROJECT/MonkeyV2/releases/download/v2.3.0/monkey-2.3.0-x86_64-linux-gnu.tar.gz
-  chmod 775 monkey-2.3.0-x86_64-linux-gnu.tar.gz
-  tar -xvzf monkey-2.3.0-x86_64-linux-gnu.tar.gz
-  cd ./monkey-2.3.0/bin
-  sudo chmod 775 *
-  sudo mv ./monkey* /usr/bin
-  #read
-  cd ~
-  rm -rfd ~/monkey_tmp
+   ## COMPILE AND INSTALL
+   if [ -d "$CONF_DIR_TMP" ]; then
+      rm -rfd $CONF_DIR_TMP
+   fi
 
-  sudo apt-get install -y ufw
-  sudo ufw allow ssh/tcp
-  sudo ufw limit ssh/tcp
-  sudo ufw logging on
-  echo "y" | sudo ufw enable
-  sudo ufw status
+   mkdir -p $CONF_DIR_TMP   
+   cd $CONF_DIR_TMP   
+   
+   wget ${URL}
+   chmod 775 ${WALLETDL}
+   tar -xvzf ${WALLETDL}
+   cd ./${WALLETDLFOLDER}/bin
+   sudo chmod 775 *
+   sudo mv ./${NAME}* /usr/bin
+   #read
+   cd ~
+   rm -rfd $CONF_DIR_TMP   
 
-  mkdir -p ~/bin
-  echo 'export PATH=~/bin:$PATH' > ~/.bash_aliases
-  source ~/.bashrc
+   sudo apt-get install -y ufw
+   sudo ufw allow ssh/tcp
+   sudo ufw limit ssh/tcp
+   sudo ufw logging on
+   echo "y" | sudo ufw enable
+   sudo ufw status
+
+   mkdir -p ~/bin
+   echo 'export PATH=~/bin:$PATH' > ~/.bash_aliases
+   source ~/.bashrc
 fi
 
 ## Setup conf
 mkdir -p ~/bin
 rm ~/bin/masternode_config.txt &>/dev/null &
-IP=$(curl -s4 icanhazip.com)
-NAME="monkey"
 COUNTER=1
 
 MNCOUNT=""
@@ -102,195 +145,230 @@ while ! [[ $MNCOUNT =~ $re ]] ; do
    read REBOOTRESTART
 done
 
-for i in `seq 1 1 $MNCOUNT`; do 
-  for (( ; ; ))
-  do  
-    echo "************************************************************"
-    echo ""
-    #echo "Enter alias for new node. Name must be unique! (Don't use same names as for previous nodes on old chain if you didn't delete old chain folders!)"
-	echo -e "${YELLOW}Enter alphanumeric alias for new node. Name must be unique!${NC}"
-    read ALIAS 
+for (( ; ; ))
+do  
+   echo "************************************************************"
+   echo ""
+   #echo "Enter alias for new node. Name must be unique! (Don't use same names as for previous nodes on old chain if you didn't delete old chain folders!)"
+   echo -e "${YELLOW}Enter alphanumeric alias for new nodes. Name must be unique!${NC}"
+   read ALIAS 
 
-    ALIAS=${ALIAS,,}  
-  
-    if [[ "$ALIAS" =~ [^0-9A-Za-z]+ ]] ; then
+   ALIAS=${ALIAS,,}  
+
+   if [[ "$ALIAS" =~ [^0-9A-Za-z]+ ]] ; then
       echo -e "${RED}$ALIAS has characters which are not alphanumeric. Please use only alphanumeric characters.${NC}"
-	elif [ -z "$ALIAS" ]; then
-	  echo -e "${RED}$ALIAS in empty!${NC}"
-    else
-	  CONF_DIR=~/.${NAME}_$ALIAS
-	  
+   elif [ -z "$ALIAS" ]; then
+      echo -e "${RED}$ALIAS in empty!${NC}"
+   else
+      CONF_DIR=~/.${NAME}_$ALIAS
       if [ -d "$CONF_DIR" ]; then
-        echo -e "${RED}$ALIAS is already used. $CONF_DIR already exists!${NC}"
+         echo -e "${RED}$ALIAS is already used. $CONF_DIR already exists!${NC}"
       else
-		# OK !!!
-        break
+         # OK !!!
+         break
       fi	
-    fi  
-  done
-  
-  PORT=""
-  RPCPORT=""
-  echo ""
-  # echo "Enter port for node $ALIAS (Any valid free port matching config from steps before: i.E. 37234)"
-  # read PORT
-  
-  if [ -z "$PORT" ]; then
-    PORT=37234
-	RPCPORT=9234
-	PORT1=""
-    for (( ; ; ))
-    do
-	  PORT1=$(netstat -peanut | grep -i monkeyd | grep -i $PORT)
-
-	  if [ -z "$PORT1" ]; then
-		break
-	  else
-		PORT=$[PORT + 1]
-		RPCPORT=$[RPCPORT + 1]
-	  fi
-    done  
-  fi
-  echo "PORT "$PORT
-
-  if [ -z "$RPCPORT" ]; then
-    echo ""
-    echo "Enter RPC Port (Any valid free port: i.E. 9234)"
-    read RPCPORT
-  fi
-  
-  echo "RPCPORT "$RPCPORT
-
-  PRIVKEY=""
-  echo ""
-  # echo "Enter masternode private key for node $ALIAS"
-  # read PRIVKEY
-
-  CONF_FILE=monkey.conf
-  
-  if [[ "$COUNTER" -lt 2 ]]; then
-    ALIASONE=$(echo $ALIAS)
-  fi  
-  echo "ALIASONE="$ALIASONE
-
-  # Create scripts
-  echo '#!/bin/bash' > ~/bin/${NAME}d_$ALIAS.sh
-  echo "${NAME}d -daemon -conf=$CONF_DIR/${NAME}.conf -datadir=$CONF_DIR "'$*' >> ~/bin/${NAME}d_$ALIAS.sh
-  echo "${NAME}-cli -conf=$CONF_DIR/${NAME}.conf -datadir=$CONF_DIR "'$*' > ~/bin/${NAME}-cli_$ALIAS.sh
-  chmod 755 ~/bin/${NAME}*.sh
-
-  mkdir -p $CONF_DIR
-  echo "rpcuser=user"`shuf -i 100000-10000000 -n 1` >> ${NAME}.conf_TEMP
-  echo "rpcpassword=pass"`shuf -i 100000-10000000 -n 1` >> ${NAME}.conf_TEMP
-  echo "rpcallowip=127.0.0.1" >> ${NAME}.conf_TEMP
-  echo "rpcport=$RPCPORT" >> ${NAME}.conf_TEMP
-  echo "listen=1" >> ${NAME}.conf_TEMP
-  echo "server=1" >> ${NAME}.conf_TEMP
-  echo "daemon=1" >> ${NAME}.conf_TEMP
-  echo "logtimestamps=1" >> ${NAME}.conf_TEMP
-  echo "maxconnections=256" >> ${NAME}.conf_TEMP
-
-  echo "" >> ${NAME}.conf_TEMP
-  echo "port=$PORT" >> ${NAME}.conf_TEMP
-  
-  if [ -z "$PRIVKEY" ]; then
-    echo ""
-  else
-    echo "masternode=1" >> ${NAME}.conf_TEMP
-    echo "masternodeprivkey=$PRIVKEY" >> ${NAME}.conf_TEMP
-  fi
-
-  sudo ufw allow $PORT/tcp
-  mv ${NAME}.conf_TEMP $CONF_DIR/monkey.conf
- 
-  if [ -z "$PRIVKEY" ]; then
-	MONKPID=`ps -ef | grep -i $ALIASONE | grep -i monkeyd | grep -v grep | awk '{print $2}'`
-	
-	if [ -z "$MONKPID" ]; then
-      # start wallet
-      sh ~/bin/${NAME}d_$ALIASONE.sh  
-	  sleep 1
-	fi
-  
-	for (( ; ; ))
-	do  
-	  echo "Please wait ..."
-      sleep 3
-	  PRIVKEY=$(~/bin/monkey-cli_${ALIASONE}.sh masternode genkey)
-	  echo "PRIVKEY=$PRIVKEY"
-	  if [ -z "$PRIVKEY" ]; then
-	    echo "PRIVKEY is null"
-	  else
-	    break
-      fi
-	done
-	
-	sleep 1
-	
-	for (( ; ; ))
-	do
-		MONKPID=`ps -ef | grep -i $ALIAS | grep -i monkeyd | grep -v grep | awk '{print $2}'`
-		if [ -z "$MONKPID" ]; then
-		  echo ""
-		else
-		  #STOP 
-		  ~/bin/monkey-cli_$ALIAS.sh stop
-		fi
-		echo "Please wait ..."
-		sleep 3 # wait 3 seconds 
-		MONKPID=`ps -ef | grep -i $ALIAS | grep -i monkeyd | grep -v grep | awk '{print $2}'`
-		echo "MONKPID="$MONKPID	
-		
-		if [ -z "$MONKPID" ]; then
-		  sleep 1 # wait 3 seconds 	
-		  echo "masternode=1" >> $CONF_DIR/monkey.conf
-		  echo "masternodeprivkey=$PRIVKEY" >> $CONF_DIR/monkey.conf
-		  # sh ~/bin/${NAME}d_$ALIAS.sh		
-		  break
-	    fi
-	done
-  fi
-  
-  sleep 2
-  MONKPID=`ps -ef | grep -i $MONKNAME | grep -i monkeyd | grep -v grep | awk '{print $2}'`
-  echo "MONKPID="$MONKPID
-  
-  if [ -z "$MONKPID" ]; then
-     echo ""
-  else
-    ~/bin/monkey-cli_$ALIAS.sh stop
-	sleep 3 # wait 3 seconds 
-  fi	
-  
-  if [ -z "$MONKPID" ]; then
-    cd $CONF_DIR
-    echo "Copy BLOCKCHAIN without conf files"
-    #wget http://blockchain.monkey.vision/ -O bootstrap.zip
-    #wget http://107.191.46.178/monk/bootstrap/bootstrap.zip -O bootstrap.zip
-    wget http://167.86.97.235/monk/bootstrap/bootstrap.zip -O bootstrap.zip
-    # rm -R peers.dat 
-	rm -R ./database
-	rm -R ./blocks	
-	rm -R ./sporks
-	rm -R ./chainstate	
-    unzip  bootstrap.zip
-    rm ./bootstrap.zip
-    sh ~/bin/${NAME}d_$ALIAS.sh		
-    sleep 5 # wait 5 seconds 
-  fi		  
-
-  
-  MNCONFIG=$(echo $ALIAS $IP:$PORT $PRIVKEY "txhash" "outputidx")
-  echo $MNCONFIG >> ~/bin/masternode_config.txt
-  
-  if [[ ${REBOOTRESTART,,} =~ "y" ]] ; then
-    (crontab -l 2>/dev/null; echo "@reboot sh ~/bin/${NAME}d_$ALIAS.sh") | crontab -
-	(crontab -l 2>/dev/null; echo "@reboot sh /root/bin/${NAME}d_$ALIAS.sh") | crontab -
-	sudo service cron reload
-  fi
-  
-  COUNTER=$[COUNTER + 1]
+   fi  
 done
+
+if [ -d "$CONF_DIR_TMP" ]; then
+   rm -rfd $CONF_DIR_TMP
+fi
+
+mkdir -p $CONF_DIR_TMP   
+cd $CONF_DIR_TMP  
+echo "Copy BLOCKCHAIN without conf files"
+wget ${BOOTSTRAPURL} -O bootstrap.zip
+cd ~
+
+for STARTNUMBER in `seq 1 1 $MNCOUNT`; do 
+   for (( ; ; ))
+   do  
+      echo "************************************************************"
+      echo ""
+      EXIT='NO'
+      ALIAS="$ALIAS$STARTNUMBER"
+      ALIAS=${ALIAS,,}  
+      echo $ALIAS
+      echo "" 
+
+      # check ALIAS
+      if [[ "$ALIAS" =~ [^0-9A-Za-z]+ ]] ; then
+         echo -e "${RED}$ALIAS has characters which are not alphanumeric. Please use only alphanumeric characters.${NC}"
+         EXIT='YES'
+	   elif [ -z "$ALIAS" ]; then
+	      echo -e "${RED}$ALIAS in empty!${NC}"
+         EXIT='YES'
+      else
+	      CONF_DIR=~/.${NAME}_$ALIAS
+	  
+         if [ -d "$CONF_DIR" ]; then
+            echo -e "${RED}$ALIAS is already used. $CONF_DIR already exists!${NC}"
+            STARTNUMBER=$[STARTNUMBER + 1]
+         else
+            # OK !!!
+            break
+         fi	
+      fi  
+   done   
+
+   if [ $EXIT == 'YES' ]
+   then
+      exit 1
+   fi
+  
+   PORT1=""
+   for (( ; ; ))
+   do
+      PORT1=$(netstat -peanut | grep -i $PORT)
+
+      if [ -z "$PORT1" ]; then
+         break
+      else
+         PORT=$[PORT + 1]
+      fi
+   done  
+   echo "PORT "$PORT 
+
+   RPCPORT1=""
+   for (( ; ; ))
+   do
+      RPCPORT1=$(netstat -peanut | grep -i $RPCPORT)
+
+      if [ -z "$RPCPORT1" ]; then
+         break
+      else
+         RPCPORT=$[RPCPORT + 1]
+      fi
+   done  
+   echo "RPCPORT "$RPCPORT
+
+   PRIVKEY=""
+   echo ""
+  
+   if [[ "$COUNTER" -lt 2 ]]; then
+      ALIASONE=$(echo $ALIAS)
+   fi  
+   echo "ALIASONE="$ALIASONE
+
+   # Create scripts
+   echo '#!/bin/bash' > ~/bin/${NAME}d_$ALIAS.sh
+   echo "${NAME}d -daemon -conf=$CONF_DIR/${NAME}.conf -datadir=$CONF_DIR "'$*' >> ~/bin/${NAME}d_$ALIAS.sh
+   echo "${NAME}-cli -conf=$CONF_DIR/${NAME}.conf -datadir=$CONF_DIR "'$*' > ~/bin/${NAME}-cli_$ALIAS.sh
+   chmod 755 ~/bin/${NAME}*.sh
+
+   mkdir -p $CONF_DIR
+   echo "rpcuser=user"`shuf -i 100000-10000000 -n 1` >> ${NAME}.conf_TEMP
+   echo "rpcpassword=pass"`shuf -i 100000-10000000 -n 1` >> ${NAME}.conf_TEMP
+   echo "rpcallowip=127.0.0.1" >> ${NAME}.conf_TEMP
+   echo "rpcport=$RPCPORT" >> ${NAME}.conf_TEMP
+   echo "listen=1" >> ${NAME}.conf_TEMP
+   echo "server=1" >> ${NAME}.conf_TEMP
+   echo "daemon=1" >> ${NAME}.conf_TEMP
+   echo "logtimestamps=1" >> ${NAME}.conf_TEMP
+   echo "maxconnections=256" >> ${NAME}.conf_TEMP
+
+   echo "" >> ${NAME}.conf_TEMP
+   echo "port=$PORT" >> ${NAME}.conf_TEMP
+  
+   if [ -z "$PRIVKEY" ]; then
+      echo ""
+   else
+      echo "masternode=1" >> ${NAME}.conf_TEMP
+      echo "masternodeprivkey=$PRIVKEY" >> ${NAME}.conf_TEMP
+   fi
+
+   sudo ufw allow $PORT/tcp
+   mv ${NAME}.conf_TEMP $CONF_DIR/monkey.conf
+ 
+   if [ -z "$PRIVKEY" ]; then
+	   MONKPID=`ps -ef | grep -i $ALIASONE | grep -v grep | awk '{print $2}'`
+	
+	   if [ -z "$MONKPID" ]; then
+         # start wallet
+         sh ~/bin/${NAME}d_$ALIASONE.sh  
+	      sleep 1
+	   fi
+  
+	   for (( ; ; ))
+	   do  
+	      echo "Please wait ..."
+         sleep 2
+	      PRIVKEY=$(~/bin/monkey-cli_${ALIASONE}.sh masternode genkey)
+	      echo "PRIVKEY=$PRIVKEY"
+	      if [ -z "$PRIVKEY" ]; then
+	         echo "PRIVKEY is null"
+	      else
+	         break
+         fi
+	   done
+	
+	   sleep 1
+	
+	   for (( ; ; ))
+	   do
+		   MONKPID=`ps -ef | grep -i $ALIAS | grep -v grep | awk '{print $2}'`
+		   if [ -z "$MONKPID" ]; then
+		      echo ""
+		   else
+		      #STOP 
+		      ~/bin/monkey-cli_$ALIAS.sh stop
+		   fi
+		   echo "Please wait ..."
+		   sleep 2 # wait 2 seconds 
+		   MONKPID=`ps -ef | grep -i $ALIAS | grep -v grep | awk '{print $2}'`
+		   echo "MONKPID="$MONKPID	
+		
+		   if [ -z "$MONKPID" ]; then
+		      sleep 1 # wait 1 second
+		      echo "masternode=1" >> $CONF_DIR/monkey.conf
+		      echo "masternodeprivkey=$PRIVKEY" >> $CONF_DIR/monkey.conf
+		      break
+	      fi
+	   done
+   fi
+  
+   sleep 2
+   MONKPID=`ps -ef | grep -i $MONKNAME | grep -v grep | awk '{print $2}'`
+   echo "MONKPID="$MONKPID
+  
+   if [ -z "$MONKPID" ]; then
+      echo ""
+   else
+      ~/bin/monkey-cli_$ALIAS.sh stop
+	   sleep 2 # wait 2 seconds 
+   fi	
+  
+   if [ -z "$MONKPID" ]; then
+      cd $CONF_DIR
+      echo "Copy BLOCKCHAIN without conf files"
+	   rm -R ./database
+	   rm -R ./blocks	
+	   rm -R ./sporks
+	   rm -R ./chainstate	
+      cp $CONF_DIR_TMP/bootstrap.zip .
+      unzip  bootstrap.zip
+      rm ./bootstrap.zip
+      sh ~/bin/${NAME}d_$ALIAS.sh		
+      sleep 2 # wait 2 seconds 
+   fi		  
+
+  
+   MNCONFIG=$(echo $ALIAS $IP:$PORT $PRIVKEY "txhash" "outputidx")
+   echo $MNCONFIG >> ~/bin/masternode_config.txt
+  
+   if [[ ${REBOOTRESTART,,} =~ "y" ]] ; then
+      (crontab -l 2>/dev/null; echo "@reboot sh ~/bin/${NAME}d_$ALIAS.sh") | crontab -
+	   (crontab -l 2>/dev/null; echo "@reboot sh /root/bin/${NAME}d_$ALIAS.sh") | crontab -
+	   sudo service cron reload
+   fi
+  
+   COUNTER=$[COUNTER + 1]
+done
+
+if [ -d "$CONF_DIR_TMP" ]; then
+   rm -rfd $CONF_DIR_TMP
+fi
+
 echo ""
 echo -e "${YELLOW}****************************************************************"
 echo -e "**Copy/Paste lines below in Hot wallet masternode.conf file**"
